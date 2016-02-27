4d87197b323ac5a1eadf2a0e05784522b493df82
C S <sethuchitra@live.com>
2016-02-26 16:21:05 -0800

    Swap all odd and even bits.

---
+++ SwapAllOddandEvenBits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapAllOddandEvenBits
+{
+    //Swap all odd and even bits.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            //int x = 10;
+            int swapped = SwapOddEven(x);
+            Console.WriteLine("Original number {0}. Swapped number {1}",x,swapped);
+            
+        }
+
+        public static int SwapOddEven(int x)
+        {
+            //int y = x >> 1;
+            //int x1 = x << 1;
+            int swap = x >> 1 | x << 1;
+             return swap;
+        }
+    }
+}
